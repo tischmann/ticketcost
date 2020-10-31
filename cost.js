2c902056327e321009b9236af2b84cc4851d17bd
Tischmann <44409532+tischmann@users.noreply.github.com>
2020-10-31 11:22:16 +0500

    Create cost.js

---
+++ cost.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const getTicketCost = () => {
+    const cost = parseFloat(document.getElementById('cost').value);
+    const distance = parseFloat(document.getElementById('distance').value);
+    const result = document.getElementById('result');
+
+    const getDiscount = (percentage) => {
+        return cost * percentage / 100;
+    };
+
+    let totalCost = 0;
+
+    if (distance <= 999) {
+        totalCost += cost * distance;
+    } else if (distance > 999) {
+        totalCost += cost * 999;
+    }
+
+    if (distance >= 1000 && distance <= 1999) {
+        totalCost += getDiscount(10) * (distance - 999);
+    } else if (distance > 1999) {
+        totalCost += getDiscount(10) * 1000;
+    }
+
+    if (distance >= 2000) {
+        totalCost += getDiscount(20) * (distance - 1999);
+    }
+
+    result.innerHTML = `Ticket cost is ${totalCost} TJS`;
+};
